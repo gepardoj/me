b11b3a9138720dc0bdb8d914d4c8274bb895d5dc
gepardoj <anna.ra.phoenix@gmail.com>
2025-05-02 00:03:09 +0300

    added stack section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,12 +22,31 @@ export default function Home() {
         </div>
       </header>
       <main className="grow">
-        <section>
+        <section className="flex flex-col">
           <Bantique type="1" />
-          <h2 className="hl text-xl relative bottom-3">Core Stack</h2>
+          <h2 className="hl text-xl relative bottom-3 mr-auto">Core Stack</h2>
+          <div className="flex flex-wrap justify-center gap-4 max-w-xl self-center">
+            <pre className="hl text-xl">React</pre>
+            <pre className="hl text-md relative top-8 right-4">Next.js</pre>
+            <pre className="hl text-xl relative left-4 top-4 ">TypeScript</pre>
+            <div className="w-full"></div>
+            <pre className="hl text-md">Tailwind CSS</pre>
+            <pre className="hl text-xl relative top-4">Material UI</pre>
+            <pre className="hl text-md relative top-8">Figma</pre>
+            <div className="w-full"></div>
+            <pre className="hl text-md">Zustand</pre>
+            <pre className="hl text-md relative top-4">Tanstack/Query</pre>
+            <pre className="hl text-md">Axios</pre>
+            <div className="w-full"></div>
+            <pre className="hl text-xl">Cypress</pre>
+            <pre className="hl text-md">react-hook-form</pre>
+            <div className="w-full"></div>
+            <pre className="hl text-md relative bottom-4 right-4">git</pre>
+            <pre className="hl text-md">npm</pre>
+          </div>
         </section>
         <section className="flex flex-col gap-2">
-          <h2 className="hl text-xl">Work Exp 4 years</h2>
+          <h2 className="hl text-xl mr-auto">Work Exp 4 years</h2>
           <p className="text-md">1y Engineer in ITSM</p>
           <p className="text-md">1y FullStack (Java/Scala)</p>
           <h2 className="hl text-xl self-center">2 years Frontend Dev</h2>
