50eee88101cda2caec5fa0acae56373845a2163c
gepardoj <anna.ra.phoenix@gmail.com>
2025-05-03 00:18:20 +0300

    reducing bundle size

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   basePath: process.env.NODE_ENV === 'production' ? '/me' : '',
   output: "export",
+  experimental: {
+    optimizeServerReact: true, // Reduces React server footprint
+    clientRouterFilter: true, // Removes unused client components
+  },
 };
 
 export default nextConfig;
